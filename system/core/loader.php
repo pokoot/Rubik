6a9ee42b4135cd0963444b29f66396c20ea22cd6
Kim <pokoot@gmail.com>
2013-08-24 23:18:56 +0800

    test

---
+++ system/core/loader.php
@@ -0,0 +1,21 @@
+<?php
+
+    require_once SYSTEM_PATH . "helper/yaml.php";
+    require_once SYSTEM_PATH . "library/spyc.php";    
+    require_once SYSTEM_PATH . "library/config.php";     
+
+    $CONFIG = new Library\Config();
+
+    $CONFIG->load( APPLICATION_PATH . "config/config.yml" , true );
+  
+
+    $environment = $CONFIG->item( array( "framework" , "environment" ) );
+ 
+
+    $path = APPLICATION_PATH . "config/" .  $environment . ".yml" ;
+ 
+    $CONFIG->load( $path , $merge = true ); 
+
+    $CONFIG->load( APPLICATION_PATH . "config/menu.yml"  , $merge = true ); 
+ 
+?>
