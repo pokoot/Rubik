a1e7a4b801d88281fca6da619e022a6b733080c4
Kim <pokoot@gmail.com>
2013-08-28 13:29:15 +0800

    require directory restructure

--- system/library/admin.php
+++ system/library/admin.php
@@ -15,9 +15,43 @@ if ( !defined('BASE_PATH')) exit('No direct script access allowed.');
  */
 class Admin{
 
+    public $model;
+    public $css;
+    public $js;
+    
+    public $vendor;
+    public $helper;
+
+
+    public $request;
+    public $yaml;
+    public $config;
+    public $language;
+
    
 
     public function index(){
+
+
+        $file = SYSTEM_PATH . "controller/" . MODULE . ".php";
+
+        if( !file_exists( $file ) ){
+            $file = APPLICATION_PATH . "admin/controller/" . MODULE . ".php" ;
+        }
+
+        if( !file_exists( $file ) ){
+            die( "Unable to load module. Please check your admin controller if it does exist. ");
+        }
+
+        require_once $file;
+
+        if( !class_exists( MODULE ) ){
+            die( "Unable to load module. The class already exist. Please check for possible class duplicates" );
+        }
+
+        
+
+        
     }
 }
 
