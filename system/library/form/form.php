2b26d5630431ea99c69553f40b8baa1d29dc236a
Kim <pokoot@gmail.com>
2013-09-06 18:53:50 +0800

    test

--- system/library/form/form.php
+++ system/library/form/form.php
@@ -6,15 +6,24 @@ if ( !defined('BASE_PATH')) exit('No direct script access allowed.');
 
 class Form{
 
+    private $config_listing;
+    private $config_entry;
+
     public $yaml;
 
     public function __construct( $yaml ){
         $this->yaml = $yaml;
+
+        $config = yaml_load_string( $yaml );
+
+        $this->config_listing   = $config["listing"];
+        $this->config_entry     = $config["entry"];
+
     }
 
     public function listing(){
 
-        $listing = new \Library\Form\Listing( $this->yaml );
+        $listing = new \Library\Form\Listing( $this->config_listing );
 
         $listing->query();
         
